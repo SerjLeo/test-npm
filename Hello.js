4949ea3cbecd40a9ba4198d04619f46e1c7d1487
SerjLeoDev <sergejleontev111@gmail.com>
2020-12-20 14:49:21 +0300

    added some langs

--- Hello.js
+++ Hello.js
@@ -1,13 +1,23 @@
 const greetings = {
-  rus: 'Привет!',
-  esp: 'Hola!',
+  ru: 'Привет!',
+  es: 'Hola!',
+  uk: 'Здрастуйте!',
+  gr: 'Χαίρετε!',
+  it: 'Ciao!',
+  cn: '你好!',
+  jp: 'こんにちは!',
+  se: 'Hallå!',
+  cz: 'Ahoj!',
+  in: 'नमस्ते!',
+  fr: 'Bonjour!',
+  fi: 'Hei!',
   default: 'Hello!'
 }
 
 const sayHello = (lang) => {
   const keys = Object.keys(greetings);
   if(!lang || typeof lang !== 'string') return greetings[keys[Math.round(Math.random() * (keys.length-1))]];
-  return greetings[lang] || greetings["default"];
+  return greetings[lang.toLowerCase()] || greetings["default"];
 }
 
 module.exports = sayHello;
--- README.md
+++ README.md
@@ -7,7 +7,17 @@
 Say "Hello" at any language you want! Just pass language abbreviation to function or get random "Hello"!
 
 ##### Available languages:
-* eng - _English_
-* rus - _Русский_
-* esp - _Espanol_
+* en - _English_
+* ru - _Русский_
+* es - _Español_
+* uk - _Український_
+* gr - _Ελληνικά_
+* it - _Italiano_
+* cn - _中文_
+* jp - _日本人_
+* se - _Svenska_
+* cz - _čeština_
+* in - _हिंदी_
+* fr - _Français_
+* fi - _Suomalainen_
 
